363ae7f8fb517122754f57ed2e47d5ab15927315
Enzo <169556594+enzo963@users.noreply.github.com>
2026-08-08 04:29:41 +0300

    Add initial HTML structure, CSS styles, and JavaScript functions for calculator

--- main.js
+++ main.js
@@ -1,17 +1,72 @@
+// variables
+
+const display = document.querySelectorAll(".result-area");
+let currentValue = "";
+const buttons = document.querySelectorAll(".btn");
 
 
-function add() {
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+
+        if (currentValue.includes(".")) {
+
+        }
+        else {
+            currentValue += event.target.textContent;
+            document.querySelector(".result-area").value = currentValue;
+        }
+    })
+});
+
+
+
+
+
+// function add() {
+
+//     }
+
+// function subtract() {
+
+//     }
+
+// function multiply() {
+
+//     }
+
+// function divide() {
+
+//     }
+
+
+// function clearDisplay() {
+// 
+// }
+// 
+// function backspace() {
+// 
+// 
+// }
+
+//-----------------------------
+
+function doNathing() {
 
 }
 
-function subtract() {
 
-}
+buttons.forEach(button => {
 
-function multiply() {
+    button.addEventListener("click", () => {
 
-}
+        button.classList.add("pressed");
 
-function divide() {
+        setTimeout(() => {
+            button.classList.remove("pressed");
+        }, 800);
 
-}
+    });
+
+});
